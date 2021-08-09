7e2f6b41639fb211162c53beb79c8ea23bbcbccd
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2021-08-09 09:36:56 -0600

    removed scripts not needed

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    GameObject[] guns;
+    public GameObject[] guns;
     // Start is called before the first frame update
     void Start()
     {
